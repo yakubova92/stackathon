ea2041c3c687a39ca13e3f7b11282ea9447fc8ad
Alla Yakubova <yakubova92@gmail.com>
2018-11-04 18:43:24 -0500

    added procfile and updated port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const passport = require('passport')
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const db = require('./db')
 const sessionStore = new SequelizeStore({db})
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
 const app = express()
 const socketio = require('socket.io')
 module.exports = app
@@ -84,7 +88,7 @@ const createApp = () => {
 
 const startListening = () => {
   // start listening (and create a 'server' object representing our server)
-  const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+  const server = app.listen(port, () => console.log(`Mixing it up on port ${port}`))
 
   // set up our socket control center
   const io = socketio(server)
